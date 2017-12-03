9a8c5ff5e7b719900647c7b97e7d4324f8916e79
Ivan Makarov <ivan.s.makarov@yandex.ru>
2017-12-03 19:37:50 +0300

    add teams grid, matches grid, news article text and about text markup and styles

--- src/matches/index.php
+++ src/matches/index.php
@@ -17,7 +17,102 @@
     </div>
     <div class="content container-flex">
         <main class="main">
+            <article class="matchday article">
+                <h3 class="heading main__heading matchday__heading">
+                    Среда, 27 декабря
+                </h3>
+                <ul class="matchday__matches container-flex container-flex_grid">
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card matchday__match match-card">
 
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card matchday__match match-card">
+
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card matchday__match match-card">
+
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card matchday__match match-card">
+
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card matchday__match match-card">
+
+                        </article>
+                    </li>
+                </ul>
+            </article>
+            <article class="matchday article">
+                <h3 class="heading main__heading matchday__heading">
+                    Вторник, 26 декабря
+                </h3>
+                <ul class="matchday__matches container-flex container-flex_grid">
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card matchday__match match-card">
+
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card matchday__match match-card">
+
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card matchday__match match-card">
+
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card matchday__match match-card">
+
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card matchday__match match-card">
+
+                        </article>
+                    </li>
+                </ul>
+            </article>
+            <article class="matchday article">
+                <h3 class="heading main__heading matchday__heading">
+                    Понедельник, 25 декабря
+                </h3>
+                <ul class="matchday__matches container-flex container-flex_grid">
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card info-card_finished matchday__match match-card">
+
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card info-card_finished matchday__match match-card">
+
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card info-card_finished matchday__match match-card">
+
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card info-card_finished matchday__match match-card">
+
+                        </article>
+                    </li>
+                    <li class="container-flex__grid-item matchday__match-container">
+                        <article class="info-card info-card_finished matchday__match match-card">
+
+                        </article>
+                    </li>
+                </ul>
+            </article>
         </main>
         <?php
             require $blocks['aside'];
--- src/teams/index.php
+++ src/teams/index.php
@@ -17,7 +17,58 @@
     </div>
     <div class="content container-flex">
         <main class="main">
+            <ul class="teams-list container-flex container-flex_grid article">
+                <li class="teams-list__item container-flex__grid-item">
+                    <article class="info-card teams-list__team">
 
+                    </article>
+                </li>
+                <li class="teams-list__item container-flex__grid-item">
+                    <article class="info-card teams-list__team">
+
+                    </article>
+                </li>
+                <li class="teams-list__item container-flex__grid-item">
+                    <article class="info-card teams-list__team">
+
+                    </article>
+                </li>
+                <li class="teams-list__item container-flex__grid-item">
+                    <article class="info-card teams-list__team">
+
+                    </article>
+                </li>
+                <li class="teams-list__item container-flex__grid-item">
+                    <article class="info-card teams-list__team">
+
+                    </article>
+                </li>
+                <li class="teams-list__item container-flex__grid-item">
+                    <article class="info-card teams-list__team">
+
+                    </article>
+                </li>
+                <li class="teams-list__item container-flex__grid-item">
+                    <article class="info-card teams-list__team">
+
+                    </article>
+                </li>
+                <li class="teams-list__item container-flex__grid-item">
+                    <article class="info-card teams-list__team">
+
+                    </article>
+                </li>
+                <li class="teams-list__item container-flex__grid-item">
+                    <article class="info-card teams-list__team">
+
+                    </article>
+                </li>
+                <li class="teams-list__item container-flex__grid-item">
+                    <article class="info-card teams-list__team">
+
+                    </article>
+                </li>
+            </ul>
         </main>
     </div>
     <?php
