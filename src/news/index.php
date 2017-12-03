4f304158ed6a0195b23c9c34bdbcea3fb4d866b7
Ivan Makarov <ivan.s.makarov@yandex.ru>
2017-12-03 20:09:20 +0300

    add news preview

--- src/news/index.php
+++ src/news/index.php
@@ -17,7 +17,144 @@
     </div>
     <div class="content container-flex">
         <main class="main">
-
+            <ul class="news-list container-flex container-flex_grid article">
+                <li class="news-list__item container-flex__grid-item">
+                    <article class="news-preview">
+                        <section class="news-preview__image-container container-flex">
+                            <img src="/img/slider-1.jpg" alt="" class="news-preview__image">
+                        </section>
+                        <h3 class="news-preview__heading">
+                            Длинное название новости в&nbsp;две или три строки с&nbsp;фиксированным  размером по&nbsp;высоте
+                        </h3>
+                        <p class="news-preview__text">
+                            Перед стартом нового сезона &laquo;Академия хоккея Ак&nbsp;Барс&raquo; проведёт аттестацию всех тренеров СДЮСШ &laquo;Ак&nbsp;Барс&raquo;, ДЮСШ &laquo;Нефтяник&raquo; и&nbsp;ДЮСШ &laquo;Динамо&raquo;.
+                        </p>
+                        <div class="container-flex container-flex_sb">
+                            <section class="news-preview__date">25.12.2017</section>
+                            <a href="news-article/" class="link news-preview__more">Читать дальше &raquo;</a>
+                        </div>
+                    </article>
+                </li>
+                <li class="news-list__item container-flex__grid-item">
+                    <article class="news-preview">
+                        <section class="news-preview__image-container container-flex">
+                            <img src="/img/slider-1.jpg" alt="" class="news-preview__image">
+                        </section>
+                        <h3 class="news-preview__heading">
+                            Длинное название новости в&nbsp;две или три строки с&nbsp;фиксированным  размером по&nbsp;высоте
+                        </h3>
+                        <p class="news-preview__text">
+                            Перед стартом нового сезона &laquo;Академия хоккея Ак&nbsp;Барс&raquo; проведёт аттестацию всех тренеров СДЮСШ &laquo;Ак&nbsp;Барс&raquo;, ДЮСШ &laquo;Нефтяник&raquo; и&nbsp;ДЮСШ &laquo;Динамо&raquo;.
+                        </p>
+                        <div class="container-flex container-flex_sb">
+                            <section class="news-preview__date">25.12.2017</section>
+                            <a href="news-article/" class="link news-preview__more">Читать дальше &raquo;</a>
+                        </div>
+                    </article>
+                </li>
+                <li class="news-list__item container-flex__grid-item">
+                    <article class="news-preview">
+                        <section class="news-preview__image-container container-flex">
+                            <img src="/img/slider-1.jpg" alt="" class="news-preview__image">
+                        </section>
+                        <h3 class="news-preview__heading">
+                            Длинное название новости в&nbsp;две или три строки с&nbsp;фиксированным  размером по&nbsp;высоте
+                        </h3>
+                        <p class="news-preview__text">
+                            Перед стартом нового сезона &laquo;Академия хоккея Ак&nbsp;Барс&raquo; проведёт аттестацию всех тренеров СДЮСШ &laquo;Ак&nbsp;Барс&raquo;, ДЮСШ &laquo;Нефтяник&raquo; и&nbsp;ДЮСШ &laquo;Динамо&raquo;.
+                        </p>
+                        <div class="container-flex container-flex_sb">
+                            <section class="news-preview__date">25.12.2017</section>
+                            <a href="news-article/" class="link news-preview__more">Читать дальше &raquo;</a>
+                        </div>
+                    </article>
+                </li>
+                <li class="news-list__item container-flex__grid-item">
+                    <article class="news-preview">
+                        <section class="news-preview__image-container container-flex">
+                            <img src="/img/slider-1.jpg" alt="" class="news-preview__image">
+                        </section>
+                        <h3 class="news-preview__heading">
+                            Длинное название новости в&nbsp;две или три строки с&nbsp;фиксированным  размером по&nbsp;высоте
+                        </h3>
+                        <p class="news-preview__text">
+                            Перед стартом нового сезона &laquo;Академия хоккея Ак&nbsp;Барс&raquo; проведёт аттестацию всех тренеров СДЮСШ &laquo;Ак&nbsp;Барс&raquo;, ДЮСШ &laquo;Нефтяник&raquo; и&nbsp;ДЮСШ &laquo;Динамо&raquo;.
+                        </p>
+                        <div class="container-flex container-flex_sb">
+                            <section class="news-preview__date">25.12.2017</section>
+                            <a href="news-article/" class="link news-preview__more">Читать дальше &raquo;</a>
+                        </div>
+                    </article>
+                </li>
+                <li class="news-list__item container-flex__grid-item">
+                    <article class="news-preview">
+                        <section class="news-preview__image-container container-flex">
+                            <img src="/img/slider-1.jpg" alt="" class="news-preview__image">
+                        </section>
+                        <h3 class="news-preview__heading">
+                            Длинное название новости в&nbsp;две или три строки с&nbsp;фиксированным  размером по&nbsp;высоте
+                        </h3>
+                        <p class="news-preview__text">
+                            Перед стартом нового сезона &laquo;Академия хоккея Ак&nbsp;Барс&raquo; проведёт аттестацию всех тренеров СДЮСШ &laquo;Ак&nbsp;Барс&raquo;, ДЮСШ &laquo;Нефтяник&raquo; и&nbsp;ДЮСШ &laquo;Динамо&raquo;.
+                        </p>
+                        <div class="container-flex container-flex_sb">
+                            <section class="news-preview__date">25.12.2017</section>
+                            <a href="news-article/" class="link news-preview__more">Читать дальше &raquo;</a>
+                        </div>
+                    </article>
+                </li>
+                <li class="news-list__item container-flex__grid-item">
+                    <article class="news-preview">
+                        <section class="news-preview__image-container container-flex">
+                            <img src="/img/slider-1.jpg" alt="" class="news-preview__image">
+                        </section>
+                        <h3 class="news-preview__heading">
+                            Длинное название новости в&nbsp;две или три строки с&nbsp;фиксированным  размером по&nbsp;высоте
+                        </h3>
+                        <p class="news-preview__text">
+                            Перед стартом нового сезона &laquo;Академия хоккея Ак&nbsp;Барс&raquo; проведёт аттестацию всех тренеров СДЮСШ &laquo;Ак&nbsp;Барс&raquo;, ДЮСШ &laquo;Нефтяник&raquo; и&nbsp;ДЮСШ &laquo;Динамо&raquo;.
+                        </p>
+                        <div class="container-flex container-flex_sb">
+                            <section class="news-preview__date">25.12.2017</section>
+                            <a href="news-article/" class="link news-preview__more">Читать дальше &raquo;</a>
+                        </div>
+                    </article>
+                </li>
+                <li class="news-list__item container-flex__grid-item">
+                    <article class="news-preview">
+                        <section class="news-preview__image-container container-flex">
+                            <img src="/img/slider-1.jpg" alt="" class="news-preview__image">
+                        </section>
+                        <h3 class="news-preview__heading">
+                            Длинное название новости в&nbsp;две или три строки с&nbsp;фиксированным  размером по&nbsp;высоте
+                        </h3>
+                        <p class="news-preview__text">
+                            Перед стартом нового сезона &laquo;Академия хоккея Ак&nbsp;Барс&raquo; проведёт аттестацию всех тренеров СДЮСШ &laquo;Ак&nbsp;Барс&raquo;, ДЮСШ &laquo;Нефтяник&raquo; и&nbsp;ДЮСШ &laquo;Динамо&raquo;.
+                        </p>
+                        <div class="container-flex container-flex_sb">
+                            <section class="news-preview__date">25.12.2017</section>
+                            <a href="news-article/" class="link news-preview__more">Читать дальше &raquo;</a>
+                        </div>
+                    </article>
+                </li>
+                <li class="news-list__item container-flex__grid-item">
+                    <article class="news-preview">
+                        <section class="news-preview__image-container container-flex">
+                            <img src="/img/slider-1.jpg" alt="" class="news-preview__image">
+                        </section>
+                        <h3 class="news-preview__heading">
+                            Длинное название новости в&nbsp;две или три строки с&nbsp;фиксированным  размером по&nbsp;высоте
+                        </h3>
+                        <p class="news-preview__text">
+                            Перед стартом нового сезона &laquo;Академия хоккея Ак&nbsp;Барс&raquo; проведёт аттестацию всех тренеров СДЮСШ &laquo;Ак&nbsp;Барс&raquo;, ДЮСШ &laquo;Нефтяник&raquo; и&nbsp;ДЮСШ &laquo;Динамо&raquo;.
+                        </p>
+                        <div class="container-flex container-flex_sb">
+                            <section class="news-preview__date">25.12.2017</section>
+                            <a href="news-article/" class="link news-preview__more">Читать дальше &raquo;</a>
+                        </div>
+                    </article>
+                </li>
+            </ul>
         </main>
         <?php
             require $blocks['aside'];
