79688d689c946bcddc6c04c6633f6b02a94ddb81
Ivan Makarov <ivan.s.makarov@yandex.ru>
2017-12-07 21:37:13 +0300

    add text to main slider

--- src/blocks/main-slider/main-slider.php
+++ src/blocks/main-slider/main-slider.php
@@ -1,8 +1,14 @@
 <div class="main-slider-container">
     <section class="main-slider owl-carousel owl-theme">
-        <article class="main-slider__item" style="background-image: url('/img/slider-1.jpg')"></article>
-        <article class="main-slider__item" style="background-image: url('/img/slider-1.jpg')"></article>
-        <article class="main-slider__item" style="background-image: url('/img/slider-1.jpg')"></article>
+        <article class="main-slider__item" style="background-image: url('/img/slider-1.jpg')">
+            <h3 class="main-slider__item-text">Академия АкБарс начала подготовку к «Турниру Академии хоккея АкБарс 2017»</h3>
+        </article>
+        <article class="main-slider__item" style="background-image: url('/img/slider-1.jpg')">
+            <h3 class="main-slider__item-text">Академия АкБарс начала подготовку к «Турниру Академии хоккея АкБарс 2017»</h3>
+        </article>
+        <article class="main-slider__item" style="background-image: url('/img/slider-1.jpg')">
+            <h3 class="main-slider__item-text">Академия АкБарс начала подготовку к «Турниру Академии хоккея АкБарс 2017»</h3>
+        </article>
     </section>
     <section class="main-slider__dots"></section>
 </div>
