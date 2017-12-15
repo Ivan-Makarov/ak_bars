5bf33e2e76d41601f13834b281d75634116f824f
Ivan Makarov <ivan.s.makarov@yandex.ru>
2017-12-15 08:07:54 +0300

    add links to team page to page teams

--- src/teams/index.php
+++ src/teams/index.php
@@ -23,78 +23,98 @@
                 ?>
                 <ul class="teams-list container-flex container-flex_grid article">
                     <li class="teams-list__item container-flex__grid-item">
-                        <article class="info-card teams-list__team team-card container-flex">
-                            <section class="team-card__badge">
-                                <div class="team-card__badge-img" style="background-image: url('/img/cska.png')"><div>
-                            </section>
-                            <div class="container-flex container-flex_col">
-                                <h3 class="team-card__heading">ЦСКА</h3>
-                                <p class="team-card__city">Москва</p>
-                                <img src="" alt="" class="team-card__flag">
-                                <p class="team-card__country">Россия</p>
-                            </div>
-                        </article>
+                        <a href="/teams/team-page" class="link-container">
+                            <article class="info-card teams-list__team team-card container-flex">
+                                <section class="team-card__badge">
+                                    <div class="team-card__badge-img" style="background-image: url('/img/cska.png')"><div>
+                                </section>
+                                <div class="container-flex container-flex_col">
+                                    <h3 class="team-card__heading">ЦСКА</h3>
+                                    <p class="team-card__city">Москва</p>
+                                    <img src="" alt="" class="team-card__flag">
+                                    <p class="team-card__country">Россия</p>
+                                </div>
+                            </article>
+                        </a>
                     </li>
                     <li class="teams-list__item container-flex__grid-item">
-                        <article class="info-card teams-list__team team-card container-flex">
-                            <section class="team-card__badge">
-                                <div class="team-card__badge-img" style="background-image: url('/img/akbars.png')"><div>
-                            </section>
-                            <div class="container-flex container-flex_col">
-                                <h3 class="team-card__heading">Ак Барс</h3>
-                                <p class="team-card__city">Казань</p>
-                                <img src="" alt="" class="team-card__flag">
-                                <p class="team-card__country">Россия</p>
-                            </div>
-                        </article>
+                        <a href="/teams/team-page" class="link-container">
+                            <article class="info-card teams-list__team team-card container-flex">
+                                <section class="team-card__badge">
+                                    <div class="team-card__badge-img" style="background-image: url('/img/akbars.png')"><div>
+                                </section>
+                                <div class="container-flex container-flex_col">
+                                    <h3 class="team-card__heading">Ак Барс</h3>
+                                    <p class="team-card__city">Казань</p>
+                                    <img src="" alt="" class="team-card__flag">
+                                    <p class="team-card__country">Россия</p>
+                                </div>
+                            </article>
+                        </a>
                     </li>
                     <li class="teams-list__item container-flex__grid-item">
-                        <article class="info-card teams-list__team team-card container-flex">
-                            <section class="team-card__badge">
-                                <div class="team-card__badge-img" style="background-image: url('/img/dynamo.png')"><div>
-                            </section>
-                            <div class="container-flex container-flex_col">
-                                <h3 class="team-card__heading">Динамо</h3>
-                                <p class="team-card__city">Москва</p>
-                                <img src="" alt="" class="team-card__flag">
-                                <p class="team-card__country">Россия</p>
-                            </div>
-                        </article>
+                        <a href="/teams/team-page" class="link-container">
+                            <article class="info-card teams-list__team team-card container-flex">
+                                <section class="team-card__badge">
+                                    <div class="team-card__badge-img" style="background-image: url('/img/dynamo.png')"><div>
+                                </section>
+                                <div class="container-flex container-flex_col">
+                                    <h3 class="team-card__heading">Динамо</h3>
+                                    <p class="team-card__city">Москва</p>
+                                    <img src="" alt="" class="team-card__flag">
+                                    <p class="team-card__country">Россия</p>
+                                </div>
+                            </article>
+                        </a>
                     </li>
                     <li class="teams-list__item container-flex__grid-item">
-                        <article class="info-card teams-list__team">
+                        <a href="/teams/team-page" class="link-container">
+                            <article class="info-card teams-list__team">
 
-                        </article>
+                            </article>
+                        </a>
                     </li>
                     <li class="teams-list__item container-flex__grid-item">
-                        <article class="info-card teams-list__team">
+                        <a href="/teams/team-page" class="link-container">
+                            <article class="info-card teams-list__team">
 
-                        </article>
+                            </article>
+                        </a>
                     </li>
                     <li class="teams-list__item container-flex__grid-item">
-                        <article class="info-card teams-list__team">
+                        <a href="/teams/team-page" class="link-container">
+                            <article class="info-card teams-list__team">
 
-                        </article>
+                            </article>
+                        </a>
                     </li>
                     <li class="teams-list__item container-flex__grid-item">
-                        <article class="info-card teams-list__team">
+                        <a href="/teams/team-page" class="link-container">
+                            <article class="info-card teams-list__team">
 
-                        </article>
+                            </article>
+                        </a>
                     </li>
                     <li class="teams-list__item container-flex__grid-item">
-                        <article class="info-card teams-list__team">
+                        <a href="/teams/team-page" class="link-container">
+                            <article class="info-card teams-list__team">
 
-                        </article>
+                            </article>
+                        </a>
                     </li>
                     <li class="teams-list__item container-flex__grid-item">
-                        <article class="info-card teams-list__team">
+                        <a href="/teams/team-page" class="link-container">
+                            <article class="info-card teams-list__team">
 
-                        </article>
+                            </article>
+                        </a>
                     </li>
                     <li class="teams-list__item container-flex__grid-item">
-                        <article class="info-card teams-list__team">
+                        <a href="/teams/team-page" class="link-container">
+                            <article class="info-card teams-list__team">
 
-                        </article>
+                            </article>
+                        </a>
                     </li>
                 </ul>
             </main>
